0cd871c7a26f74172cf5d9f38872c70be8f83bbc
Kacper Poneta <poneciak@gmail.com>
2024-09-12 19:31:22 +0200

    implemented lexer

---
+++ src/lexer.rs
@@ -0,0 +1,244 @@
+use std::mem;
+
+use miette::{Error, LabeledSpan, SourceSpan};
+
+use crate::{error::{SingleTokenError, UnterminatedStringError}, token::{Token, TokenKind}};
+
+pub struct Lexer<'de> {
+    whole: &'de str,
+    rest: &'de str,
+    byte: usize,
+    peeked: Option<Result<Token<'de>, Error>>
+}
+
+impl<'de> Lexer<'de> {
+    pub fn new(input: &'de str) -> Self {
+        Self { 
+            whole: input,
+            rest: input,
+            byte: 0,
+            peeked: None
+        }
+    }
+    pub fn peek(&mut self) -> Option<&Result<Token<'de>, Error>> {
+        if self.peeked.is_some() {
+            return self.peeked.as_ref()
+        }
+        self.peeked = self.next();
+        self.peeked.as_ref()
+    }
+}
+
+impl<'de> Iterator for Lexer<'de> 
+{
+    type Item = Result<Token<'de>, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(_) = self.peeked {
+            return mem::replace(&mut self.peeked, None);
+        }
+        loop {
+            let mut chars = self.rest.chars();
+            let c = chars.next()?;
+            let c_at = self.byte;
+            let c_str = &self.rest[..c.len_utf8()];
+            let c_onwards = self.rest;
+            self.rest = chars.as_str();
+            self.byte += c.len_utf8();
+
+            enum Started {
+                String,
+                Number,
+                Ident,
+                IfEqualElse(TokenKind, TokenKind),
+                Comment
+            }
+
+            let just = move |kind: TokenKind| {
+                Some(Ok(Token::new(kind, c_str, c_at)))
+            };
+
+            let started = match c {
+                '(' => return just(TokenKind::LEFT_PAREN),
+                ')' => return just(TokenKind::RIGHT_PAREN),
+                '{' => return just(TokenKind::LEFT_BRACE),
+                '}' => return just(TokenKind::RIGHT_BRACE),
+                '[' => return just(TokenKind::LEFT_SQUARE_BRACKET),
+                ']' => return just(TokenKind::RIGHT_SQUARE_BRACKET),
+                ',' => return just(TokenKind::COMMA),
+                '.' => return just(TokenKind::DOT),
+                '-' => return just(TokenKind::MINUS),
+                '+' => return just(TokenKind::PLUS),
+                ';' => return just(TokenKind::SEMICOLON), 
+                '*' => return just(TokenKind::STAR),
+
+                '/' => Started::Comment,
+                '<' => Started::IfEqualElse(TokenKind::LESS_EQUAL, TokenKind::LESS),
+                '>' => Started::IfEqualElse(TokenKind::GREATER_EQUAL, TokenKind::GREATER),
+                '=' => Started::IfEqualElse(TokenKind::EQUAL_EQUAL, TokenKind::EQUAL),
+                '!' => Started::IfEqualElse(TokenKind::BANG_EQUAL, TokenKind::BANG),
+                '"' => Started::String,
+                '0'..='9' => Started::Number,
+                'a'..='z' | 'A'..='Z' | '_' => Started::Ident,
+
+                c if c.is_whitespace() => continue,
+                _ => return Some(Err(SingleTokenError {
+                    src: self.whole.to_string(),
+                    token: c,
+                    err_span: SourceSpan::from(self.byte - c.len_utf8()..self.byte),
+                }.into()))
+            };
+
+
+            match started {
+                Started::String => {
+                    // TODO maybe delete escaping its unnecessery
+                    #[derive(PartialEq, Debug)]
+                    enum State {
+                        Normal,
+                        Escape,
+                        Closed
+                    }
+                    let mut state = State::Normal;
+                    let mut i = 0;
+
+                    while let Some(c) = chars.next() {
+                        i += c.len_utf8();
+                        match c {
+                            '\\' => match state {
+                                State::Escape => state = State::Normal,
+                                _ => state = State::Escape
+                            },
+                            '"' => match state {
+                                State::Escape => state = State::Normal,
+                                _ => {
+                                    state = State::Closed;
+                                    break
+                                }
+                            },
+                            _ => match state {
+                                State::Escape => state = State::Normal,
+                                _ => {}
+                            }
+                        }
+                    }
+                    let str_rep = &c_onwards[..i + c.len_utf8()];
+                    self.rest = &self.rest[i..];
+                    self.byte += i;
+                    if state != State::Closed {
+                        // string wasn't closed
+                        return Some(Err(UnterminatedStringError {
+                            src: self.whole.to_string(),
+                            err_span: SourceSpan::from(self.byte - c.len_utf8() - i..self.byte),
+                        }.into()))
+                    }
+                    return Some(Ok(Token::new(TokenKind::STRING, str_rep, c_at)));
+                },
+                Started::Number => {
+                    #[derive(PartialEq)]
+                    enum State {
+                        BeforeDot,
+                        Dot,
+                        AfterDot
+                    }
+                    let mut state = State::BeforeDot;
+                    let mut i = 0;
+                    
+                    while let Some(c) = chars.next() {
+                        match c {
+                            '0'..='9' | '_'  => {
+                                match state {
+                                    State::Dot => { state = State::AfterDot }
+                                    _ => {}
+                                }
+                            },
+                            '.' => match state {
+                                State::BeforeDot => { state = State::Dot; },
+                                _ => break
+                            },
+                            _ => break
+                        }
+                        i += c.len_utf8();
+                    }
+                    if state == State::Dot {
+                        // means last characted is a dot
+                        i -= '.'.len_utf8();
+                    }
+                    let str_rep = &c_onwards[..i + c.len_utf8()];
+                    let str_rep_normalized = str_rep.replace("_", "");
+                    self.rest = &self.rest[i..];
+                    self.byte += i;
+                    let num = match str_rep_normalized.parse() {
+                        Ok(n) => n,
+                        Err(err) => {
+                            return Some(Err(miette::miette! {
+                                labels = vec![
+                                    LabeledSpan::at(self.byte - i..self.byte, "here")
+                                ],
+                                "Failed to parse number '{str_rep}' in input\n error: {err:?}"
+                            }.with_source_code(self.whole.to_string())));
+                        },
+                    };
+                    return Some(Ok(Token::new(TokenKind::NUMBER(num), str_rep, c_at)))
+
+                },
+                Started::Ident =>  {
+                    let first_non_ident = c_onwards
+                        .find(|c| !matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_'))
+                        .unwrap_or_else(|| c_onwards.len());
+                    let str_rep = &c_onwards[..first_non_ident];
+                    let extra_bytes = str_rep.len() - c.len_utf8();
+                    self.byte += extra_bytes;
+                    self.rest = &self.rest[extra_bytes..];
+
+                    let kind = match str_rep {
+                        "and" => TokenKind::AND,
+                        "else" => TokenKind::ELSE,
+                        "false" => TokenKind::FALSE,
+                        "true" => TokenKind::TRUE,
+                        "for" => TokenKind::FOR,
+                        "fun" => TokenKind::FUN,
+                        "if" => TokenKind::IF,
+                        "nil" => TokenKind::NIL,
+                        "or" => TokenKind::OR,
+                        "return" => TokenKind::RETURN,
+                        "var" => TokenKind::VAR,
+                        "while" => TokenKind::WHILE,
+                        "print" => TokenKind::PRINT,
+                        _ => TokenKind::IDENT
+                    };
+                    return Some(Ok(Token::new(kind, str_rep, c_at)));
+                },
+                Started::Comment => {
+                    if let Some(this_c) = chars.next() {
+                        if this_c == '/' {
+                            let n_line = c_onwards
+                                .find(|c| matches!(c, '\n'))
+                                .unwrap_or_else(|| c_onwards.len());
+                            self.rest = &c_onwards[n_line..];
+                            self.byte += c_onwards.len() - self.rest.len() - c.len_utf8();
+                            continue;
+                        }
+                    }
+                    return just(TokenKind::SLASH);
+                }
+                Started::IfEqualElse(yes, no) => {
+                    self.rest = self.rest.trim_start();
+                    let trimmed = c_onwards.len() - self.rest.len() - 1;
+                    self.byte += trimmed;
+
+
+                    if self.rest.starts_with('=') {
+                        let span = &c_onwards[..c.len_utf8() + trimmed + 1];
+                        self.rest = &self.rest[1..];
+                        self.byte += 1;
+                        return Some(Ok(Token::new(yes, span, c_at)));
+                    } else {
+                        return Some(Ok(Token::new(no, c_str, c_at)))
+                    }
+                }
+            }
+            // todo!() // unreachable
+        }
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,153 @@
+use std::{borrow::Cow, fmt};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token<'de> {
+    pub kind: TokenKind,
+    pub offset: usize,
+    pub origin: &'de str,
+}
+impl<'de> Token<'de> {
+    pub fn new(kind: TokenKind, origin: &'de str, offset: usize) -> Self {
+        Self { kind, origin, offset } 
+    }
+}
+#[allow(non_camel_case_types)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum TokenKind {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    LEFT_SQUARE_BRACKET,
+    RIGHT_SQUARE_BRACKET,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    STAR,
+
+    EQUAL,
+    BANG_EQUAL,
+    EQUAL_EQUAL,
+    LESS_EQUAL,
+    GREATER_EQUAL,
+    LESS,
+    GREATER,
+    SLASH,
+    BANG,
+
+    STRING,
+    IDENT,
+    NUMBER(f64),
+
+    // keywords
+    PRINT,
+    AND,
+    ELSE,
+    FALSE,
+    FOR,
+    FUN,
+    IF,
+    NIL,
+    OR,
+    RETURN,
+    TRUE,
+    VAR,
+    WHILE,
+}
+impl fmt::Display for Token<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let kind = self.kind;
+        let i = self.origin;
+        match kind {
+            TokenKind::LEFT_PAREN => write!(f, "LEFT_PAREN {i} null"),
+            TokenKind::RIGHT_PAREN => write!(f, "RIGHT_PAREN {i} null"),
+            TokenKind::LEFT_BRACE => write!(f, "LEFT_BRACE {i} null"),
+            TokenKind::RIGHT_BRACE => write!(f, "RIGHT_BRACE {i} null"),
+            TokenKind::LEFT_SQUARE_BRACKET => write!(f, "LEFT_SQUARE_BRACKET {i} null"),
+            TokenKind::RIGHT_SQUARE_BRACKET => write!(f, "RIGHT_SQUARE_BRACKET {i} null"),
+            TokenKind::COMMA => write!(f, "COMMA {i} null"),
+            TokenKind::DOT => write!(f, "DOT {i} null"),
+            TokenKind::MINUS => write!(f, "MINUS {i} null"),
+            TokenKind::PLUS => write!(f, "PLUS {i} null"),
+            TokenKind::SEMICOLON => write!(f, "SEMICOLON {i} null"),
+            TokenKind::STAR => write!(f, "STAR {i} null"),
+            TokenKind::EQUAL => write!(f, "EQUAL {i} null"),
+            TokenKind::BANG_EQUAL => write!(f, "BANG_EQUAL {i} null"),
+            TokenKind::EQUAL_EQUAL => write!(f, "EQUAL_EQUAL {i} null"),
+            TokenKind::LESS_EQUAL => write!(f, "LESS_EQUAL {i} null"),
+            TokenKind::GREATER_EQUAL => write!(f, "GREATER_EQUAL {i} null"),
+            TokenKind::LESS => write!(f, "LESS {i} null"),
+            TokenKind::GREATER => write!(f, "GREATER {i} null"),
+            TokenKind::SLASH => write!(f, "SLASH {i} null"),
+            TokenKind::BANG => write!(f, "BANG {i} null"),
+            TokenKind::STRING => write!(f, "STRING \"{}\" {}", unescape(i), unescape(i)),
+            TokenKind::IDENT => write!(f, "IDENTIFIER {i} null"),
+            TokenKind::NUMBER(n) => write!(f, "NUMBER {i} {}", format_num(n)),
+            TokenKind::PRINT => write!(f, "PRINT {i} null"),
+            TokenKind::AND => write!(f, "AND {i} null"),
+            TokenKind::ELSE => write!(f, "ELSE {i} null"),
+            TokenKind::FALSE => write!(f, "FALSE {i} null"),
+            TokenKind::FOR => write!(f, "FOR {i} null"),
+            TokenKind::FUN => write!(f, "FUN {i} null"),
+            TokenKind::IF => write!(f, "IF {i} null"),
+            TokenKind::NIL => write!(f, "NIL {i} null"),
+            TokenKind::OR => write!(f, "OR {i} null"),
+            TokenKind::RETURN => write!(f, "RETURN {i} null"),
+            TokenKind::TRUE => write!(f, "TRUE {i} null"),
+            TokenKind::VAR => write!(f, "VAR {i} null"),
+            TokenKind::WHILE => write!(f, "WHILE {i} null"),
+        }
+    }
+}
+
+pub fn format_num(n: f64) -> String {
+    if n.fract() == 0.0 { format!("{n:.1}") } else { format!("{n}") }
+}
+
+pub fn unescape<'de>(s: &'de str) -> Cow<'de, str> {
+    // TODO change this latter with better escapings
+    let mut chars = s.chars();
+    let mut ret = String::new();
+    enum State {
+        Starting,
+        Normal,
+        Escaping,
+    }
+    let mut state = State::Starting;
+    while let Some(c) = chars.next() {
+        match c {
+            '\\' => match state {
+                State::Escaping => {
+                    state = State::Normal;
+                    ret.push('\\');
+                },
+                _ => state = State::Escaping
+            },
+            '"' => match state {
+                State::Starting => state = State::Normal,
+                State::Normal => {},
+                State::Escaping => {
+                    state = State::Normal;
+                    ret.push('"');
+                },
+            },
+            _ => match state {
+                State::Escaping => {
+                    state = State::Normal;
+                    let special = match c {
+                        'n' => '\n',
+                        'r' => '\r',
+                        't' => '\t',
+                        '\'' => '\'',
+                        _ => c
+                    };
+                    ret.push(special);
+                },
+                _ => ret.push(c)
+            }
+        }
+    }
+    Cow::Owned(ret)
+}
