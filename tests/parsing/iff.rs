736ff04540ca0c867fa47bf43facbc0cea8d878c
Kacper Poneta <poneciak@gmail.com>
2024-09-23 15:37:33 +0200

    implemented tests for "if" parsing

--- tests/parsing/iff.rs
+++ tests/parsing/iff.rs
@@ -1,4 +1,4 @@
-use super::test_parse_expr;
+use super::{test_parse, test_parse_expr, unwrap_parse};
 
 
 #[test]
@@ -9,7 +9,65 @@ fn if_expressions() {
         print 2;
         print 3;
     }", "(if true (block (print 1) (print 2) (print 3)))");
-    todo!()
 }
 
-// todo else and other tests
+#[test]
+fn if_statement() {
+    test_parse("if (1 < 3) print 1;", "(if (< 1 3) (print 1))");
+    test_parse("
+    if (true) {
+        print 1;
+        print 2;
+        print 3;
+    }
+    ", "(if true (block (print 1) (print 2) (print 3)))");
+}
+
+#[test]
+fn if_else_expressions() {
+    test_parse_expr("if (1 < 3) print 1; else print 2;", "(if (< 1 3) (print 1) (print 2))");
+    test_parse_expr("
+    if (true) {
+        print 1;
+        print 2;
+    } else {
+        print 3;
+        print 4;
+    }
+    ", "(if true (block (print 1) (print 2)) (block (print 3) (print 4)))");
+}
+
+#[test]
+fn if_else_statement() {
+    test_parse("if (1 < 3) print 1; else print 2;", "(if (< 1 3) (print 1) (print 2))");
+    test_parse("
+    if (true) {
+        print 1;
+        print 2;
+    } else {
+        print 3;
+        print 4;
+    }
+    ", "(if true (block (print 1) (print 2)) (block (print 3) (print 4)))");
+}
+
+#[test]
+#[should_panic]
+fn if_incorrect_syntax1() {
+    unwrap_parse("if (true)");
+}
+#[test]
+#[should_panic]
+fn if_incorrect_syntax2() {
+    unwrap_parse("if (true) else");
+}
+#[test]
+#[should_panic]
+fn if_incorrect_syntax3() {
+    unwrap_parse("if else print 1;");
+}
+#[test]
+#[should_panic]
+fn if_incorrect_syntax4() {
+    unwrap_parse("if (true) print 1; else");
+}
